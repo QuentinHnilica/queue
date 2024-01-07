70e15ec9aa084e670b43bd7e434d3e70961c31b3
Quentin Hnilica <QuentinHnilica@gmail.com>
2024-01-07 13:40:32 -0500

    Updated form styles, updated logo, updating some images (wip)

--- public/assets/js/contact.js
+++ public/assets/js/contact.js
@@ -1,62 +1,61 @@
-const sub = document.querySelector('#Sub')
-const name1 = document.querySelector('#name')
-const email = document.querySelector('#email')
-const phone = document.querySelector('#phone')
-const message = document.querySelector('#message')
+const sub = document.querySelector("#Sub");
+const name1 = document.querySelector("#name");
+const email = document.querySelector("#email");
+const phone = document.querySelector("#phone");
+const message = document.querySelector("#message");
 
-function validateFourm(){
-    let errors = 0
+function validateFourm() {
+  let errors = 0;
 
-    if (name1.value == null || name1.value == ""){
-        errors++
-        name1.placeholder = 'Name Must Be Filled'
-    }
+  if (name1.value == null || name1.value == "") {
+    errors++;
+    name1.placeholder = "Name Must Be Filled";
+  }
 
-    if (email.value == null || email.value == ""){
-        errors++
-        email.placeholder = 'email Must Be Filled'
-    }
+  if (email.value == null || email.value == "") {
+    errors++;
+    email.placeholder = "email Must Be Filled";
+  }
 
-    if (phone.value == null || phone.value.length < 10){
-        errors++
-        phone.value = ""
-        phone.placeholder = 'phone Must Valid'
-    }
+  if (phone.value == null || phone.value.length < 10) {
+    errors++;
+    phone.value = "";
+    phone.placeholder = "phone Must Valid";
+  }
 
-    if (message.value == null || message.value == ""){
-        errors++
-        message.placeholder = 'message Must Be Filled'
-    }
+  if (message.value == null || message.value == "") {
+    errors++;
+    message.placeholder = "message Must Be Filled";
+  }
 
-    return errors
+  return errors;
 }
 
-const newMessage = async () =>{
+const newMessage = async (e) => {
+  e.preventDefault();
+  let theErrors = validateFourm();
+  console.log(theErrors);
 
-    let theErrors = validateFourm()
+  if (theErrors === 0) {
+    let newMessage = {
+      name: name1.value,
+      email: email.value,
+      phone: phone.value,
+      message: message.value,
+    };
 
-    if (theErrors === 0) {
-        let newMessage = {
-            name: name1.value,
-            email: email.value,
-            phone: phone.value,
-            message: message.value
-        }
+    const response = await fetch("/contact/submit", {
+      method: "POST",
+      body: JSON.stringify(newMessage),
+      headers: { "Content-Type": "application/json" },
+    });
 
-        const response = await fetch('/contact/submit', {
-            method: "POST",
-            body: JSON.stringify(newMessage),
-            headers: { 'Content-Type': 'application/json' },
-        })
-
-        if (response.ok){
-            console.log("group Created")
-        }
-        else{
-            alert(response.statusText)
-        }
+    if (response.ok) {
+      console.log("group Created");
+    } else {
+      alert(response.statusText);
     }
+  }
+};
 
-}
-
-sub.addEventListener('click', newMessage)
+sub.addEventListener("click", newMessage);
