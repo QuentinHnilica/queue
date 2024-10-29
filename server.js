0161d4359408919e49dea4cdbca21f5b41137ec5
unknown <quentinhnilica@gmail.com>
2024-10-28 21:44:52 -0400

    Port

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3004;
 
 const hbs = exphbs.create({
   extname: ".handlebars",
