e25c16b912f88d7db840e10fdc2fbb0b2294df96
unknown <quentinhnilica@gmail.com>
2025-03-06 12:13:01 -0500

    login Error Testing

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,19 +1,27 @@
-const loginFormHandler = async () => {
+const loginFormHandler = async (e) => {
+
+  event.preventDefault(); // Prevent default form submission
 
   const userName = document.querySelector("#user-id").value.trim();
   const password = document.querySelector("#password-input").value.trim();
 
   if (userName && password) {
-    const response = await fetch("/login", {
-      method: "POST",
-      body: JSON.stringify({ userName, password }),
-      headers: { "Content-Type": "application/json" },
-    });
+    try {
+      const response = await fetch("/login", {
+        method: "POST",
+        body: JSON.stringify({ userName, password }),
+        headers: { "Content-Type": "application/json" },
+      });
 
-    if (response.ok) {
-      document.location.replace("/admin");
-    } else {
-      alert("Failed to log in!");
+      if (response.ok) {
+        document.location.replace("/admin");
+      } else {
+        const errorMsg = await response.text();
+        alert("Login failed: " + errorMsg);
+      }
+    } catch (err) {
+      console.error("Error during login:", err);
+      alert("Error logging in. Check console for details.");
     }
   }
 };
