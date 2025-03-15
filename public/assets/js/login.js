b09b03400b859892e99d33369ab00ef97e02013b
unknown <quentinhnilica@gmail.com>
2025-03-15 16:57:14 -0400

    All Body pages will have a parent div that sets the height of the page to 100 percent to ensure footer stays at the bottom of the page

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -14,7 +14,6 @@ const loginFormHandler = async (e) => {
       });
 
       if (response.ok) {
-        alert("YouLoggedIn!")
         document.location.replace("/admin");
       } else {
         const errorMsg = await response.text();
