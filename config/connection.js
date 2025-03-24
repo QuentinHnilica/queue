2ed5dc1ff4a7bba61dc99c9e704ca31e574c8183
unknown <quentinhnilica@gmail.com>
2025-03-23 22:41:40 -0400

    additional connection change

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,14 @@ require("dotenv").config();
 
 const sequelize = new Sequelize(process.env.SQLDBNAME, process.env.SQLUSER, process.env.SQLPASS, {
   host: process.env.SQLHOST,
-  port: 3306,
-  dialect: "mysql",
+  port: 5432,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // necessary for Supabase
+    },
+  },
 });
 
 module.exports = sequelize;
