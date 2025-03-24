efa489ba0594b832662987149d4a7fd543e30390
unknown <quentinhnilica@gmail.com>
2025-03-23 22:29:15 -0400

    Connectionjs change

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(process.env.SQLDBNAME, process.env.SQLUSER, process.env.SQLPASS, {
-  host: "localhost",
+  host: process.env.SQLHOST,
   port: 3306,
   dialect: "mysql",
 });
