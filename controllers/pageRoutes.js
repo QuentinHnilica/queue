8d40371b63f0ca44956b1020bcf83d6ed1fa5c99
QuentinHnilica <Quentinhnilica@gmail.com>
2025-03-31 15:55:29 -0400

    Form fully completed

--- controllers/pageRoutes.js
+++ controllers/pageRoutes.js
@@ -78,7 +78,15 @@ router.post("/contact/submit", async (req, res) => {
     res.status(400).json(err);
   }
 });
-
+router.post("/consult/submit", async (req, res) => {
+  try {
+    console.log(req.body);
+    const newMess = await Leads.create(req.body);
+    res.status(200).json(newMess);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 // Disclosure Routes
 router.get("/privacy", async (req, res) => {
   res.render("privacy");
